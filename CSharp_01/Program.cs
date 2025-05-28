c2c4be5c8da933ca602c61fa77d2f390704efc49
akayrailhan <alikayra.ilhannn@gmail.com>
2025-05-28 18:20:37 +0300

    Add project files.

---
+++ CSharp_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region write
+
+            //Console.WriteLine("Hello World!");
+            //Console.Write("Hi Bros");
+
+            #endregion
+
+            string name;
+            name = "John Doe";
+
+            Console.WriteLine("Hello " + name);
+
+            string customerSurname = "Smith";
+
+            Console.WriteLine(customerSurname);
+
+            Console.Read();
+        }
+    }
+} 
